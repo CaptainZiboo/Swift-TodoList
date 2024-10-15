e7add12ec9dc8d8737f020a488ee1bf505a2cebc
Lucas POYART <lucas.poyart@edu.devinci.fr>
2024-10-15 15:22:09 +0200

    wip: auth system

---
+++ TodoList/AuthController.swift
@@ -0,0 +1,8 @@
+//
+//  AuthController.swift
+//  TodoList
+//
+//  Created by POYART Lucas on 15/10/2024.
+//
+
+import Foundation
---
+++ TodoList/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  TodoList
+//
+//  Created by POYART Lucas on 15/10/2024.
+//
+
+import Foundation
