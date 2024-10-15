ba99953e5ee0a60376691475d0173e4ec1b132d1
Lucas POYART <lucas.poyart@edu.devinci.fr>
2024-10-15 13:37:04 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by POYART Lucas on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
